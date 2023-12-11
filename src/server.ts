c9d15ee07f6118deb4fb317b3686395bc9803827
Nikita <82110006+NikitaMelnychencko@users.noreply.github.com>
2023-12-11 12:17:39 +0200

    fix/port in server

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app'
 const port = process.env.PORT || '8080';
 
 app.listen(port, async () => {
-  console.log('Server running. Use our API on port: 8080');
+  console.log(`Server running. Use our API on port: ${port}`);
 });
